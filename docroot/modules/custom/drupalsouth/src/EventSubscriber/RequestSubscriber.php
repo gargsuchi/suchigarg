20cc193dd59ed76730e1bf4bdd9d5eaca45ce172
gargsuchi <gargsuchi@gmail.com>
2019-11-20 04:36:35 +0000

    Subscriber

--- docroot/modules/custom/drupalsouth/src/EventSubscriber/RequestSubscriber.php
+++ docroot/modules/custom/drupalsouth/src/EventSubscriber/RequestSubscriber.php
@@ -4,6 +4,7 @@ namespace Drupal\drupalsouth\EventSubscriber;
 namespace Drupal\drupalsouth\EventSubscriber;
 
 use Drupal\alexa\AlexaEvent;
+use Drupal\views\Views;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -29,14 +30,77 @@ class RequestSubscriber implements EventSubscriberInterface {
     $request = $event->getRequest();
     $response = $event->getResponse();
 
-    switch ($request->intentName) {
-      case 'AMAZON.HelpIntent':
-        $response->respond('You can ask anything and I will respond with "Hello Drupal"');
-        break;
+    $type = get_class($request);
 
-      default:
-        $response->respond('Hello Drupal');
-        break;
+    if ($type == "Alexa\Request\SessionEndedRequest") {
+          $response->respond('Okay. Bye.')
+            ->endSession();
+    }
+    elseif ($type == "Alexa\Request\LaunchRequest") {
+      // The skill was just launched, so welcome the user and provide help.
+
+      // A somewhat contrived example of an SSML Alexa response, demonstrating a
+      // few ways you can customize Alexa's annunciation. This could instead
+      // be done simply with a plain ->respond('Welcome! ...') to use Alexa's
+      // default annunciation.
+      $response->respondSSML('<speak><say-as interpret-as="interjection">Welcome!</say-as>
+      Say <prosody volume="loud">What can I make </prosody><break strength="medium"/>
+      with <emphasis level="strong">an ingredient</emphasis><break strength="medium"/>
+      and I\'ll respond with a list of recipes.</speak>');
+    }
+    elseif ($type == "Alexa\Request\IntentRequest") {
+      switch ($request->intentName) {
+        case 'CaptureIngredientIntent':
+          // Get the {Ingredient} slot's value.
+          $ingredient = $request->getSlot('Ingredient');
+          $view = Views::getView('recipe_list');
+          $view->setDisplay('block_1');
+          $view->setArguments([ $ingredient ]);
+          $view->execute();
+          $result = [];
+          if (empty($view->build_info['fail']) and empty($view->build_info['denied'])) {
+            $result = $view->result;
+            $response_text = '<speak><say-as interpret-as="interjection">' . "These are the recipes for $ingredient" . '</say-as><break strength="medium"/>';
+if (count($result)) {
+          foreach($result AS $id => $row) {
+            foreach ($view->field as $fid => $field) {
+              if ($fid == 'title') {
+		$response_text .= $field->getValue($row) . '<break strength="medium"/>';
+	      }
+	    }
+          }
+$response_text .= '</speak>';
+          }
+else {
+            $response_text = "<speak>Sorry. I did not find any recipes for $ingredient.</speak>";
+
+          }
+}
+            $response->respondSSML($response_text);
+          break;
+
+        case 'AMAZON.HelpIntent':
+          $response->respond('You can say "What can I make with paneer", and I will list recipes for paneer');
+          break;
+
+        case 'AMAZON.ExitIntent':
+          $response->respond('Okay. Bye.')
+            ->endSession();
+          break;
+
+        default:
+          $response->respond('Welcome to Drupal Cooking! Which ingredient would you like to search recipes for?');
+          break;
+      }
+    }
+    elseif ($request instanceof SessionEndedRequest) {
+      // @todo: Clean up any saved session state here.
+    }
+    else {
+      \Drupal::logger('alexa_demo')
+        ->warning('Request was not an expected request type: @type', [
+          '@type' => get_class($request),
+        ]);
     }
   }
 
